4c7c87a837301776439612fda6080fd8f5c61e80
Peter Huang <miterfrnats@gmail.com>
2021-09-07 00:02:48 +0800

    first commit

---
+++ Constants/IotRole.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel;
+using Homo.AuthApi;
+
+namespace Homo.IotApi
+{
+    public enum IOT_ROLE
+    {
+        NO = ROLE.NO,
+        ADMIN = ROLE.ADMIN,
+
+
+
+    }
+}
---
+++ Models/EF/IotDbContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Homo.IotApi
+{
+    public partial class IotDbContext : DbContext
+    {
+        public IotDbContext() { }
+
+        public IotDbContext(DbContextOptions<IotDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+
+    }
+}
