30883df7f1dfdbe7311da6462193b7ae2a222c40
Peter Huang <miterfrnats@gmail.com>
2021-09-25 08:24:08 +0800

    feat: update ef device model allow zoneId null

--- Models/EF/Device.cs
+++ Models/EF/Device.cs
@@ -13,7 +13,7 @@ namespace Homo.IotApi
         [Required]
         [MaxLength(64)]
         public string Name { get; set; }
-        public long ZoneId { get; set; }
+        public long? ZoneId { get; set; }
         public Zone Zone { get; set; }
         public string Info { get; set; }
         [MaxLength(128)]
